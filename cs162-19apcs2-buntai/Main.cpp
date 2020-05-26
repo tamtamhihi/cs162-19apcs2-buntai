c5adc418facefd8402fb7c5acda63afb070602ef
LeThaoHuyen <lethaohuyen3010@gmail.com>
2020-05-26 21:05:13 +0700

    Add main.cpp demo

--- cs162-19apcs2-buntai/Main.cpp
+++ cs162-19apcs2-buntai/Main.cpp
@@ -1,5 +1,13 @@
 #include "Function.h"
 
 int main() {
+	cout << "\t\t\t===============BUNTAI SCHOOL MANAGEMENT=============\n\n";
+	string userName;
+	int userRole;
+	while (!login(userName, userRole)){
+		clearScreen();
+	}
+	clearScreen();
+	showAllRoleMenu(userName, userRole);
 	return 0;
 }
